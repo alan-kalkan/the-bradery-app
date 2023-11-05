20d89142da89e610ca540c13c8f935e35a8d6a7c
Alan <alan0613@live.fr>
2023-11-05 17:22:32 +0100

    feat: Created CartProduct component and dynamic cart

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Button, Container, Navbar, Modal } from "react-bootstrap";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { CartContext } from "../CartContext";
+import CartProduct from "./CartProduct";
 
 function NavbarComponent() {
+  const cart = useContext(CartContext);
+
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const productsCount = cart.items.reduce(
+    (sum, product) => sum + product.quantity,
+    0
+  );
+
   return (
     <>
       <Navbar expand="sm">
         <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Button onClick={handleShow}>Cart 0 items</Button>
+          <Button onClick={handleShow}>Cart ({productsCount} items)</Button>
         </Navbar.Collapse>
       </Navbar>
       <Modal show={show} onHide={handleClose}>
@@ -20,7 +30,22 @@ function NavbarComponent() {
           <Modal.Title>Shopping Cart</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h1>Modal Body</h1>
+          {productsCount > 0 ? (
+            <>
+              <p>Items in your cart :</p>
+              {cart.items.map((currentProduct, index) => (
+                <CartProduct
+                  key={index}
+                  id={currentProduct.id}
+                  quantity={currentProduct.quantity}
+                />
+              ))}
+              <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
+              <Button variant="success">Purchase items!</Button>
+            </>
+          ) : (
+            <h1>No items in your cart</h1>
+          )}
         </Modal.Body>
       </Modal>
     </>
